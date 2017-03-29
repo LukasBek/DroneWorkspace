e38491622645cb794f1fc7fe15bbb17393446825
nickinylin <nickinylin@hotmail.com>
2017-03-29 10:19:22 +0200

    hej

--- src/Image_converter.cpp
+++ src/Image_converter.cpp
@@ -14,6 +14,8 @@ class ImageConverter
   image_transport::Subscriber image_sub_;
   image_transport::Publisher image_pub_;
   
+  int counter = 0;
+  
 public:
   ImageConverter()
     : it_(nh_)
@@ -31,12 +33,13 @@ public:
     cv::destroyWindow(OPENCV_WINDOW);
   }
 
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+void imageCb(const sensor_msgs::ImageConstPtr& msg)
   {
     cv_bridge::CvImagePtr cv_ptr;
     try
     {
       cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      
     }
     catch (cv_bridge::Exception& e)
     {
@@ -53,7 +56,7 @@ public:
     cv::waitKey(3);
     
     // Output modified video stream
-    image_pub_.publish(cv_ptr->toImageMsg());
+    // image_pub_.publish(cv_ptr->toImageMsg());
   }
 };
 
@@ -61,8 +64,18 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "image_converter");
   ImageConverter ic;
- while (ROS::OK()) {
+  int counter = 0;
+  ros::Rate loop_rate(50);
+ while (ros::ok()) {
 ros::spinOnce();
+
+
+
+
+
+ROS_INFO("HEJSA! %d",counter);
+      counter++;
+loop_rate.sleep();
  }
   
   return 0;
